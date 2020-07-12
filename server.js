076f56f4cd9c52178598ec6a4c424153b96f9613
feriva22 <fericodeva69@gmail.com>
2020-07-13 05:27:07 +0700

    test again

--- server.js
+++ server.js
@@ -19,5 +19,4 @@ app.get('/', function (req, res) {
 
 var server = app.listen(process.env.PORT || 5000, function () {
   var port = server.address().port;
-  console.log("App now running on port", port);
 });
